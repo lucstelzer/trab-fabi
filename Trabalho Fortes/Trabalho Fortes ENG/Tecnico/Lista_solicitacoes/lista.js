49aca3502dbf8afb79c74fb85e898729d77ae383
Lucstelzer <lucassossaistelzer@gmail.com>
2025-11-03 08:15:04 -0300

    1.8

---
+++ Trabalho Fortes/Trabalho Fortes ENG/Tecnico/Lista_solicitacoes/lista.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggle-sidebar');
+const sidebar = document.getElementById('sidebar');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
+
+function logout() {
+    localStorage.removeItem('token');
+    window.location.href = '/Trabalho Fortes/Trabalho Fortes ENG/Login/login.html';
+}
+
+document.getElementById('logout').addEventListener('click', logout);
