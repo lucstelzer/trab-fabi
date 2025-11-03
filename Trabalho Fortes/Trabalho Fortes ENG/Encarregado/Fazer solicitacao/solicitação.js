00e60ec862cf2c531d117ade4b502c088f49d6d4
Joanddes <gjoanddes@gmail.com>
2025-11-03 14:40:49 -0300

    1.9.1

--- Trabalho Fortes/Trabalho Fortes ENG/Encarregado/Fazer solicitacao/solicitação.js
+++ Trabalho Fortes/Trabalho Fortes ENG/Encarregado/Fazer solicitacao/solicitação.js
@@ -2,19 +2,35 @@ const toggleButton = document.getElementById('toggle-sidebar');
 const sidebar = document.getElementById('sidebar');
 
 toggleButton.addEventListener('click', () => {
- 
     sidebar.classList.toggle('open');
 });
+
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('solicitacao-form');
-    form.addEventListener('submit', (event) => {
-        
-        
-        event.preventDefault();
-
-        alert('Solicitação enviada com sucesso!');
-
-        form.reset();
+    
+    const element = document.getElementById('tecnicos');
+    
+    const choices = new Choices(element, {
+        removeItemButton: true,
+        placeholder: true,
+        placeholderValue: 'Digite ou selecione um técnico...',
+        searchPlaceholderValue: 'Digite para pesquisar',
+        allowHTML: false,
     });
 
+    const form = document.getElementById('solicitacao-form');
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const querEnviar = confirm('Deseja mesmo fazer essas alterações?');
+
+        if (querEnviar) {
+            alert('Solicitação enviada com sucesso!');
+            
+            form.reset(); 
+            choices.clearStore();
+        } else {
+            
+        }
+    });
 });
